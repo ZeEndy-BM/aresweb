e36aa99816683a43e71196a85e60630cea796d26
swag gamer <z33ndy@gmail.com>
2021-11-24 14:32:16 +0200

    Update adminpanel.php

--- adminpanel.php
+++ adminpanel.php
@@ -28,59 +28,33 @@ $queryRecords = mysqli_query($conn, $sql) or die("error to fetch user database")
          <th>PASSWORD</th>
       </tr>
    </thead>
+   <form name="form" method="post">
    <tbody id="_editable_table">
       <?php foreach($queryRecords as $res) :?>
-      <tr data-row-id="<?php echo $res['id'];?>">
-         <td class="editable-col" contenteditable="true" col-index='0' oldVal ="<?php echo $res['id'];?>"><?php echo $res['id'];?></td>
-         <td class="editable-col" contenteditable="true" col-index='1' oldVal ="<?php echo $res['username'];?>"><?php echo $res['username'];?></td>
-         <td class="editable-col" contenteditable="true" col-index='2' oldVal ="<?php echo $res['password'];?>"><?php echo $res['password'];?></td>
+      <tr>
+         <td><?php echo $res['id'];?></td>
+         <td  name="username" oldVal ="<?php echo $res['username'];?>"><input type="text"><?php echo $res['username'];?></td>
+         <td contenteditable="true" name="password" oldVal ="<?php echo $res['password'];?>"><?php echo $res['password'];?></td>
+         <td><input name="id" value="<?php echo $res['id']; ?>" type="submit"></td>
       </tr>
     <?php endforeach;?>
    </tbody>
+   </form>
 </table>
 </body>
 </html>
 <?php
-  $columns = array(
-    0 =>'id',
-    1 => 'username',
-    2 => 'password'
-  );
-  $error = true;
-  $colVal = '';
-  $colIndex = $rowId = 0;
-  
-  $msg = array('status' => !$error, 'msg' => 'Failed! updation in mysql');
- 
-  if(isset($_POST)){
-    if(isset($_POST['val']) && !empty($_POST['val']) && $error) {
-      $colVal = $_POST['val'];
-      $error = false;
-      
-    } else {
-      $error = true;
-    }
-    if(isset($_POST['index']) && $_POST['index'] >= 0 &&  $error) {
-      $colIndex = $_POST['index'];
-      $error = false;
-    } else {
-      $error = true;
-    }
-    if(isset($_POST['id']) && $_POST['id'] > 0 && $error) {
-      $rowId = $_POST['id'];
-      $error = false;
-    } else {
-      $error = true;
-    }
-  
-    if(!$error) {
-        $sql = "UPDATE user_db SET ".$columns[$colIndex]." = '".$colVal."' WHERE id='".$rowId."'";
-        $status = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-        $msg = array('status' => !$error, 'msg' => 'Success! updation in mysql');
-    }
-  }
-  
-  // send data as json format
-  echo json_encode($msg);
+
+
+if(isset($_POST['id']) && $_POST['id'] > 0) {
+  $id = (int)$_POST['id'];
+  echo $_POST['id'];
+//  $sql = "UPDATE user_db SET id='".$id."' WHERE ";
+}
+
+//while($row = mysqli_fetch_assoc($r)){
+ // var_dump($row);
+ // echo "<hr>";
+//}
 
 ?>
